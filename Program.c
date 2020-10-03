9107ed340446e12ad66b529a332ff150cd8354ad
Subhasmita <57298668+Ask-Subhasmita@users.noreply.github.com>
2020-10-03 14:11:00 +0530

    Fixed and included scanset characters to take input.

--- Program.c
+++ Program.c
@@ -2,18 +2,23 @@
 
 int main()
 {
-    char name[20],country,skill;
+    char name[30],country,skill;
     int age;
     printf("Welcome to Hacktoberfest 2020\n");
+    
     printf("Enter your name : ");//Get User name
-    scanf("%s",&name);
+    scanf("%[^\n]%*c",&name);
+    
     printf("Enter your Country of Residance : ");//Get user country
-    scanf("%s",&country);
+    scanf("%[^\n]%*c",&country);
+    
     printf("Enter your Age : ");//Get user Age
-    printf("%d",&age);
+    scanf("%d",&age);
+    
     printf("Rate your programmings skills from A-D (A being the highest and D being the lowest : ");//Get user skill level
     scanf(" %c",&skill);
-    scanf("Thank you %s Happy Hacking",country);//Thank the user
+    
+    printf("Thank you %s Happy Hacking",name);//Thank the user
 
     return 0;
 }
